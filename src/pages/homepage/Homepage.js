9413668f04288dad09ff9274dba41c1e28be10ba
Ons Attia <onesattia@gmail.com>
2019-08-07 11:26:05 +0100

    dived homepage to components & add some styling

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+// Styles
+import "./Homepage.scss";
+// Components
+import Directory from "../../components/directory/Directory";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
