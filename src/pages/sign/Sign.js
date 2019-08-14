3aebc24578ccabd8ad0fe8210ae35603d1e9ce65
Ons Attia <onesattia@gmail.com>
2019-08-14 11:26:25 +0100

    finished signIn & signUp

--- src/pages/sign/Sign.js
+++ src/pages/sign/Sign.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 
 const Sign = () => {
   return (
     <div>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
